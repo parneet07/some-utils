7de1731027c34ffc236b766f7849aaf2a496e71c
parneet <parneettoor7@gmail.com>
2020-05-27 14:26:51 -0700

    third commit

--- utils.js
+++ utils.js
@@ -3,4 +3,8 @@ function product(x, y) {
 }
 console.log(product(process.argv[2],process.argv[3])); 
 
+ function divide(x,y){
+     return x/y
+ }
+ console.log(divide(process.argv[2],process.argv[3]));
  
